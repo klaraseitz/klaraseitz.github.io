4197670ea8366b06500a25456c88fae7ce63be47
Klara <klara.seitz@googlemail.com>
2020-07-07 19:01:48 +0200

    set up vue and vuetify

---
+++ portfolio/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <PortfolioOverview></PortfolioOverview>
+  </v-app>
+</template>
+
+<script>
+import PortfolioOverview from "./components/PortfolioOverview";
+
+export default {
+  name: 'App',
+
+  components: {
+    PortfolioOverview
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ portfolio/src/components/PortfolioOverview.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <p>
+      I'm a Software Developer with a Masters Degree from HPI, Potsdam, Germany (September 2020).
+    </p>
+    <ProjectCard></ProjectCard>
+  </div>
+</template>
+
+<script>
+import ProjectCard from "./ProjectCard";
+
+export default {
+  name: 'PortfolioOverview',
+  props: {
+    msg: String
+  },
+  components: {
+    ProjectCard
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
