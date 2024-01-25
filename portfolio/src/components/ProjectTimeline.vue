67fd713c9a043ff9704e75c86c18390431d0fe48
klaraseitz <11996418+klaraseitz@users.noreply.github.com>
2024-01-25 15:11:46 +0100

    add button for examples

--- portfolio/src/components/ProjectTimeline.vue
+++ portfolio/src/components/ProjectTimeline.vue
@@ -88,6 +88,9 @@
             <v-btn href="/crochet" text>
               <v-icon left>mdi-play-circle-outline</v-icon> Try the Prototype
             </v-btn>
+            <v-btn href="/examples" text>
+              <v-icon left>mdi-download</v-icon> Download examples to open in the prototype
+            </v-btn>
             <v-btn href="https://github.com/klaraseitz/LayerwiseCrochet" text>
               <v-icon left>mdi-code-json</v-icon> View the Code
             </v-btn>
